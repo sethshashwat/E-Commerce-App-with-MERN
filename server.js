6fa9c7afa5867f8d84ecb9fdc81f3035fb841ca8
Shashwat Seth <shashwat.seth.131999@gmail.com>
2020-09-22 20:12:25 +0530

    Changes to be committed:

--- server.js
+++ server.js
@@ -16,9 +16,6 @@ app.use(routes)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
 }
 
 mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
